570f20719501fa1246a4d018ef49b5cc3d721a58
pabloabur <pabloabur@gmail.com>
2020-06-16 19:42:55 +1000

    added ranger configs and removed termite

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -12,13 +12,23 @@ rm ~/.vimrc
 rm ~/.zshrc
 rm ~/.tmux.conf
 rm ~/.config/i3/config
-rm ~/.config/termite/config
 rm ~/.config/zathura/zathurarc
+
+rm ~/.config/ranger/commands.py
+rm ~/.config/ranger/commands_full.py
+rm ~/.config/ranger/rc.conf
+rm ~/.config/ranger/rifle.conf
+rm ~/.config/ranger/scope.sh
 
 # Creating symlinks
 ln -s $dir/vim/vimrc ~/.vimrc
 ln -s $dir/zsh/zshrc ~/.zshrc
 ln -s $dir/tmux/tmux.conf ~/.tmux.conf
 ln -s $dir/i3/config ~/.config/i3/config
-ln -s $dir/termite/config ~/.config/termite/config
 ln -s $dir/zathura/zathurarc ~/.config/zathura/zathurarc
+
+ln -s $dir/ranger/commands.py ~/.config/ranger/commands.py
+ln -s $dir/ranger/commands_full.py ~/.config/ranger/commands_full.py
+ln -s $dir/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -s $dir/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -s $dir/ranger/scope.sh ~/.config/ranger/scope.sh
