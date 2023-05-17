79a27d94590a940a4e389b258a584bf8d81a9f71
pabloabur <pabloabur@gmail.com>
2023-05-17 13:29:16 +1000

    aliases

---
+++ zsh/aliases.sh
@@ -0,0 +1,8 @@
+alias gts='git status'
+alias gtc='git commit -m'
+alias gtco='git checkout'
+alias gta='git add'
+alias gtam='git status | tee /dev/tty | grep modified | awk '{print $2}' | dmenu | xargs git add'
+alias gtat="git status | grep -A 50 '(use ' | awk 'NR>2 {print last} {last=$0}'"
+alias gtpl='git pull'
+alias gtph='git push origin'
