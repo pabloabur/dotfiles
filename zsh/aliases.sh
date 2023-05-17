1d3cc2edf97b9b5b4f76f97854c262b3cc53c8e1
pabloabur <pabloabur@gmail.com>
2023-05-17 13:57:03 +1000

    adds functionality for gitmergetool

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -6,3 +6,4 @@ alias gtam='git status | tee /dev/tty | grep modified | awk '{print $2}' | dmenu
 alias gtat="git status | grep -A 50 '(use ' | awk 'NR>2 {print last} {last=$0}'"
 alias gtpl='git pull'
 alias gtph='git push origin'
+alias gtmt='git mergetool --tool=nvimdiff2'
