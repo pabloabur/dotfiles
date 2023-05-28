3064a4b0e8257f07bdd7e530d64ee4ea43a8142d
pabloabur <pabloabur@gmail.com>
2023-05-28 15:36:58 +1000

    Separates aliases and add log aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,10 +1,22 @@
 alias gts='git status'
+
 alias gtc='git commit -m'
+
 alias gtco='git checkout'
+
 alias gta='git add'
 alias gtam='git status | tee /dev/tty | grep modified | awk '\''{print $2}'\'' | dmenu-mac | xargs git add'
 alias gtat='git status | grep -A 50 '\''Untracked files:'\'' | awk '\''NR>3 {print last} {last=$0}'\'' | dmenu-mac | xargs git add'
-alias gtd='git status | tee /dev/tty | grep --color=always modified | awk '\''{print $2}'\'' | dmenu-mac | xargs git diff'
+
+alias gtl='git log -p'
+alias gtls='git log --stat'
+alias gtlg='git log --oneline --decorate --all --graph'
+
+alias gtd='git diff'
+alias gtdm='git status | tee /dev/tty | grep --color=always modified | awk '\''{print $2}'\'' | dmenu-mac | xargs git diff'
+
 alias gtpl='git pull'
+
 alias gtph='git push origin'
+
 alias gtmt='git mergetool --tool=nvimdiff2'
