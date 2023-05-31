86946bafc11f8c65b6abda16923f53e16203abda
pabloabur <pabloabur@gmail.com>
2023-05-31 17:58:11 +1000

    Replaces dmenu by fzf

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -5,15 +5,14 @@ alias gtc='git commit -m'
 alias gtco='git checkout'
 
 alias gta='git add'
-alias gtam='git status | tee /dev/tty | grep modified | awk '\''{print $2}'\'' | dmenu-mac | xargs git add'
-alias gtat='git status | grep -A 50 '\''Untracked files:'\'' | awk '\''NR>3 {print last} {last=$0}'\'' | dmenu-mac | xargs git add'
+alias gtam='git status | grep modified | awk '\''{print $2}'\'' | fzf-tmux -m -p | xargs git add'
+alias gtat='git status | grep -A 50 '\''Untracked files:'\'' | awk '\''NR>3 {print last} {last=$0}'\'' | fzf-tmux -m -p | xargs git add'
 
 alias gtl='git log -p'
 alias gtls='git log --stat'
 alias gtlg='git log --oneline --decorate --all --graph'
 
-alias gtd='git diff'
-alias gtdm='git status | tee /dev/tty | grep --color=always modified | awk '\''{print $2}'\'' | dmenu-mac | xargs git diff'
+alias gtd='git status | grep modified | awk '\''{print $2}'\'' | fzf-tmux -m -p | xargs git diff'
 
 alias gtpl='git pull'
 
