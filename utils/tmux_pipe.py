b4ee361d8141f373b069df0ae2e34a50aae834b0
pabloabur <pabloabur@gmail.com>
2023-09-28 18:59:41 +1000

    Adds script folder

--- utils/tmux_pipe.py
+++ utils/tmux_pipe.py
@@ -3,6 +3,8 @@ import subprocess
 import textwrap
 import sys
 
+pane_id = sys.argv[1]
+
 # Get the code from the clipboard
 code = subprocess.check_output('pbpaste' if sys.platform == 'darwin' else 'xclip -o -selection clipboard', shell=True, text=True)
 
@@ -11,5 +13,5 @@ dedented_code = textwrap.dedent(code)
 
 # Send the dedented code to the Tmux panel
 subprocess.run(f'tmux set-buffer "{dedented_code}"', shell=True)
-subprocess.run('tmux paste-buffer -t %7 ; tmux send-keys -t %7 Enter', shell=True)
+subprocess.run(f'tmux paste-buffer -t %{pane_id} ; tmux send-keys -t %{pane_id} Enter', shell=True)
 
