e1b5d8bce6c7707f4544e68e3a796be8832ff4f1
pabloabur <pabloabur@gmail.com>
2024-07-24 16:31:55 +1000

    Improves get_diff script
    Now it is more reliable to get the right line (at least so far). I
    also found a way to create a new line with the correct number when
    a new paragraph is added. Situations like this were being lost before

--- utils/get_diff_tex.sh
+++ utils/get_diff_tex.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 modified_file="$1"
 
-
- git diff --unified=0 --word-diff HEAD^ "$modified_file" | grep -E '@@' | sed -E 's/@@ -([0-9]*) \+[0-9]* @@/\1/g' | awk '{print $1}' > line_numbers
+git diff --unified=0 --word-diff HEAD~8 "$modified_file" | grep -E '@@' | sed -E 's/@@ -[0-9]*(,[0-9])? \+([0-9]*(,[0-9])?) @@/\2/g' | awk '{print $1}' | awk -F, '{if (NF == 2) {print $1; print $1 + $2 - 1} else {print $0}}' > line_numbers
+# Sometimes there is , on diff output, in which case +(0-9)* is more precise (dont know why)
+# When there are a new line, added text does not show up with line number. I then kept the ,count part and add a new line number with awk
 
 git diff --unified=0 --word-diff HEAD^ "$modified_file" | grep -E '\-\]|\+\}' | sed -e 's/\[-/\\textcolor{red}{/g' -e 's/-]/}/g' -e 's/{+/\\textcolor{blue}{/g' -e 's/+}/}/g' > line_modifications
 
@@ -13,4 +14,5 @@ paste line_numbers line_modifications | while read -r number content; do
 done
 
 rm line_numbers line_modifications
+# TODO probably does not work when line breaks are involved
 # TODO Problems when inside $. Might need to manually delete middle $ and fix last }, maybe adding a new \textcolo{}{ if changes are over text and math
